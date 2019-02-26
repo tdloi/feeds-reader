31bc545e54519691673cde477ac3690bd504e8e5
Loi Tran <tdloi@protonmail.com>
2019-02-27 02:16:13 +0700

    Add function wrap around fetch to ensure return JSON response

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,23 @@ export function getListSites(page) {
   }
   return JSON.parse(list);
 }
+
+// ensure response data will always in JSON format or
+// return undefined in case error
+export async function fetchData(url, type) {
+  const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+
+  if (type === 'reddit') {
+    return response.json().catch(err => undefined);
+  }
+
+  const { Parser } = await import('xml2js');
+  // parseString does not return value directly
+  // see https://github.com/Leonidas-from-XIV/node-xml2js/issues/380
+  let _result;
+  new Parser({explicitArray: false}).parseString(
+    await response.text(), (err, result) => { _result = result;
+  });
+
+  return _result;
+}
